e7fb8c270622216eec37ed7255a6eed5ac65abf6
Dmitry <example@gmail.com>
2022-04-28 11:39:09 +0700

    add if-else, switch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
+/* let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
 console.log(numberOfFilms);
 
 const personalMovieDB = {
@@ -19,3 +19,44 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+ */
+
+
+
+/* 
+if (4 == 9) {
+    console.log('ok');
+} else {
+console.log('Error');
+} */
+
+/* const num = 50;
+
+if (num<49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log("Много");
+}else {
+    console.log('OK!');
+}
+
+(num === 50) ? console.log('OK!') : console.log('Error'); */
+
+const num = 50;
+
+switch (num) {
+    case 49: 
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 51:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+
+
+}
